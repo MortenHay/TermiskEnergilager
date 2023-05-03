e24847dff37ebfcb17223a01db66ee874edbeaf0
Morten <33462750+MortenHay@users.noreply.github.com>
2023-05-03 21:56:32 +0200

    Banger

--- simulation.py
+++ simulation.py
@@ -12,42 +12,105 @@ innerBucket_height = df_vars.loc["innerBucket_height"]["val"]
 outerBucket_height = df_vars.loc["outerBucket_height"]["val"]
 conduction_rockwool = df_vars.loc["conduction_rockwool"]["val"]
 conduction_glasswool = df_vars.loc["conduction_glasswool"]["val"]
+conduction_PP = df_vars.loc["conduction_PP"]["val"]
 convection_lid = df_vars.loc["convection_lid"]["val"]
 convection_side = df_vars.loc["convection_side"]["val"]
 innerBucket_radius = df_vars.loc["innerBucket_radius"]["val"]
 outerBucket_radius = df_vars.loc["outerBucket_radius"]["val"]
 convection_bottom = df_vars.loc["convection_bottom"]["val"]
 specific_heat_water = df_vars.loc["specific_heat_water"]["val"]
+width_bucket = df_vars.loc["width_bucket"]["val"]
 
 # %% Resistance equation
 side = sp.Symbol("s")
-resistance_conduction_side = sp.ln((innerBucket_radius + side) / innerBucket_radius) / (
-    2 * sp.pi * innerBucket_height * conduction_rockwool
+resistance_conduction_side_RW = sp.ln(
+    (innerBucket_radius + side) / innerBucket_radius
+) / (2 * sp.pi * innerBucket_height * conduction_rockwool)
+
+resistance_conduction_side_B1 = sp.ln(
+    (innerBucket_radius) / innerBucket_radius - width_bucket
+) / (2 * sp.pi * innerBucket_height * conduction_PP)
+
+resistance_conduction_side_B2 = sp.ln(
+    (outerBucket_radius) / innerBucket_radius - width_bucket
+) / (2 * sp.pi * outerBucket_height * conduction_PP)
+
+resistance_total_side = (
+    resistance_conduction_side_RW
+    + resistance_conduction_side_B1
+    + resistance_conduction_side_B2
 )
-resistance_convection_side = 1 / (convection_side * 2 * sp.pi * outerBucket_radius * outerBucket_height)
-resistance_total_side = resistance_conduction_side + resistance_convection_side
 
 lid = sp.Symbol("l")
-resistance_conduction_lid = lid / (
+resistance_conduction_lid_RW = lid / (
     conduction_rockwool * sp.pi * outerBucket_radius**2
 )
-resistance_convection_lid = 1 / (convection_lid * sp.pi * outerBucket_radius**2)
-resistance_total_lid = resistance_conduction_lid + resistance_convection_lid
+
+resistance_conduction_lid_B1 = width_bucket / (
+    conduction_PP * sp.pi * innerBucket_radius**2
+)
+
+resistance_conduction_lid_B2 = width_bucket / (
+    conduction_PP * sp.pi * outerBucket_radius**2
+)
+
+resistance_total_lid = (
+    resistance_conduction_lid_RW
+    + resistance_conduction_lid_B1
+    + resistance_conduction_lid_B2
+)
 
 bottom = sp.Symbol("b")
-resistance_conduction_bottom = bottom / (
+resistance_conduction_bottom_RW = bottom / (
     conduction_rockwool * sp.pi * outerBucket_radius**2
 )
-resistance_convection_bottom = 1 / (convection_bottom * sp.pi * outerBucket_radius**2)
-resistance_total_bottom = resistance_conduction_bottom + resistance_convection_bottom
 
-resistance_total = 1 / (
-    1 / resistance_total_bottom + 1 / resistance_total_side + 1 / resistance_total_lid
+resistance_conduction_bottom_B1 = width_bucket / (
+    conduction_PP * sp.pi * innerBucket_radius**2
 )
+
+resistance_conduction_bottom_B2 = width_bucket / (
+    conduction_PP * sp.pi * outerBucket_radius**2
+)
+
+resistance_total_bottom = (
+    resistance_conduction_bottom_RW
+    + resistance_conduction_bottom_B1
+    + resistance_conduction_bottom_B2
+)
+
+
+def resistance_convection(deltaT):
+    h = 1.42 * ((deltaT / (outerBucket_height * 1000)) ** 0.25)
+    A = (
+        2 * sp.pi * outerBucket_radius**2
+        + 2 * sp.pi * outerBucket_radius * outerBucket_height
+    )
+    return 1 / (h * A)
+
+
+def resistance_radiation(t1, t2):
+    sigma = 5.67e-8
+    emissivity = 0.03
+    return 1 / (sigma * emissivity * (t1**2 + t2**2) * (t1 + t2))
+
+
+def resistance_total(tHigh, tLow):
+    resistance_conduction = 1 / (
+        1 / resistance_total_bottom
+        + 1 / resistance_total_lid
+        + 1 / resistance_total_side
+    )
+
+    resistance_outer = 1 / (
+        1 / resistance_convection(tHigh - tLow) + 1 / resistance_radiation(tHigh, tLow)
+    )
+
+    return resistance_conduction + resistance_outer
 
 
 # %% Differential equation
-def heatTransfer(Tin, Tout, R, C):
+def heatTransfer(Tin, Tout, C):
     """
     Parameters
     ----------
@@ -60,19 +123,20 @@ def heatTransfer(Tin, Tout, R, C):
     --------
     qx : Heat flow [W]
     """
-    qx = (Tout - Tin) / R
+    qx = (Tout - Tin) / resistance_total(Tout, Tin)
     dT = qx / C
     return dT
 
+
 def warmUp(U, I, C):
-    q=U*I
-    dT=q/C
+    q = U * I
+    dT = q / C
     return dT
 
 
-def warmUp_heatTransfer(Tin, Tout, R, U, I, C):
-    return heatTransfer(Tin, Tout, R, C) + warmUp(U, I, C)
-    
+def warmUp_heatTransfer(Tin, Tout, U, I, C):
+    return heatTransfer(Tin, Tout, C) + warmUp(U, I, C)
+
 
 def eulersMethod(de, x0: float, y0: float, h: float, xn: float, *args):
     """
@@ -104,23 +168,22 @@ def eulersMethod(de, x0: float, y0: float, h: float, xn: float, *args):
 # %% Main code
 
 # Thermal resistance and heat capacity for simulation'
-l = 0.051
-s = outerBucket_radius-innerBucket_radius
-b = 0.051
+lid = 0.051
+side = outerBucket_radius - innerBucket_radius
+bottom = 0.051
 
-resistance_thermal = resistance_total.subs({lid: l, bottom: b, side: s})
 mass_water = 3.21  # kg
 heat_capacity = specific_heat_water * mass_water
 
 # Simulation
 start_time = 0
 step_size = 10  # s
-end_time = 60*60*24*5  # s
+end_time = 60 * 60 * 24 * 5  # s
 outside_temperature = 23.4  # C
 start_temperature = outside_temperature  # C
-voltage = 30 #V
-amperage = 6 #A
-switchTime = 47.5*60 #s
+voltage = 30  # V
+amperage = 6  # A
+switchTime = 47.5 * 60  # s
 
 t, T = eulersMethod(
     warmUp_heatTransfer,
@@ -129,7 +192,6 @@ t, T = eulersMethod(
     step_size,
     switchTime,
     outside_temperature,
-    resistance_thermal,
     voltage,
     amperage,
     heat_capacity,
@@ -142,14 +204,13 @@ t2, T2 = eulersMethod(
     step_size,
     end_time,
     outside_temperature,
-    resistance_thermal,
     heat_capacity,
 )
 
-t = np.concatenate((t,t2),axis=0)
-T= np.concatenate((T,T2),axis=0)
+t = np.concatenate((t, t2), axis=0)
+T = np.concatenate((T, T2), axis=0)
 
-print('Simulation complete!')
+print("Simulation complete!")
 
 # %% Export data
 df_sim = pd.DataFrame({"t": t, "T": T})
