01a7286c9efed64de5c59bbf215fd52dedf21648
Morten <haymorten@gmail.com>
2023-05-21 12:10:36 +0200

    Banger

--- plotting.py
+++ plotting.py
@@ -11,7 +11,7 @@ df_sim = df_sim.set_index("t")
 df_param = pd.read_csv("param.csv")
 df_param = df_param.set_index("param")
 
-df_run = pd.read_csv('imports/run1.csv')
+df_run = pd.read_csv('imports/run2.csv')
 df_run['Time'] = pd.to_datetime(df_run["Time"])
 df_run['dt'] = (df_run['Time'] - df_run['Time'].shift(1)).dt.total_seconds()
 df_run['dt'].iloc[0] = 0
@@ -20,20 +20,23 @@ df_run=df_run.set_index('t')
 
 
 # %% Plots
-plt.plot(df_sim.index / 3600, df_sim["T"], label="Tank temperature")
+plt.plot(df_sim.index/3600, df_sim["T"], label="Simulation Tank")
+plt.plot(df_sim.index/3600, df_sim["TT"], label="Simulation Surface")
 
-probes = ["top", "mid","bot", "room"]
+
+probes = ["top", "surface","bot", "room"]
 for i in range(1,5):
-    plt.plot(df_run.index / 3600, df_run["Probe-{}".format(i)], label="Probe {}".format(probes[i-1]))
+    plt.plot(df_run.index/3600, df_run["Probe-{}".format(i)], label="Probe {}".format(probes[i-1]))
     
     
 plt.axhline(
     y=df_param.loc["outside_temperature"]["val"],
-    color="orange",
     linestyle="dashed",
     xmax=df_param.loc["end_time"]["val"],
     label="Outside Temperature",
 )
+
+plt.xlim(left=0, right=24*5)
 plt.xlabel("Time [h]")
 plt.ylabel("Temperature [C]")
 plt.title("Cooling of thermal storage tank")
--- simulation.py
+++ simulation.py
@@ -22,7 +22,7 @@ specific_heat_water = df_vars.loc["specific_heat_water"]["val"]
 width_bucket = df_vars.loc["width_bucket"]["val"]
 
 # %% Resistance equation
-side = outerBucket_radius - innerBucket_radius
+side = (outerBucket_radius - innerBucket_radius-0.005)
 resistance_conduction_side_RW = sp.ln(
     (innerBucket_radius + side) / innerBucket_radius
 ) / (2 * sp.pi * innerBucket_height * conduction_rockwool)
@@ -41,7 +41,7 @@ resistance_total_side = (
     + resistance_conduction_side_B2
 )
 
-lid = 0.051
+lid = 0.05
 resistance_conduction_lid_RW = lid / (
     conduction_rockwool * sp.pi * outerBucket_radius**2
 )
@@ -60,7 +60,7 @@ resistance_total_lid = (
     + resistance_conduction_lid_B2
 )
 
-bottom = 0.051
+bottom = 0.05
 resistance_conduction_bottom_RW = bottom / (
     conduction_rockwool * sp.pi * outerBucket_radius**2
 )
@@ -82,7 +82,7 @@ resistance_total_bottom = (
 
 def resistance_convection(deltaT):
     h = 1.42 * ((deltaT / (outerBucket_height)) ** 0.25)
-    A = (
+    A = 2.3*(
         2 * sp.pi * outerBucket_radius**2
         + 2 * sp.pi * outerBucket_radius * outerBucket_height
     ).evalf()
@@ -92,7 +92,11 @@ def resistance_convection(deltaT):
 def resistance_radiation(t1, t2):
     sigma = 5.67e-8
     emissivity = 0.03
-    return 1 / (sigma * emissivity * (t1**2 + t2**2) * (t1 + t2))
+    A = 2.3*(
+        2 * sp.pi * outerBucket_radius**2
+        + 2 * sp.pi * outerBucket_radius * outerBucket_height
+    ).evalf()
+    return (1 / (A * sigma * emissivity * (t1**2 + t2**2) * (t1 + t2)))
 
 
 def resistance_total(tHigh, tLow):
@@ -106,14 +110,14 @@ def resistance_total(tHigh, tLow):
     )
 
     resistance_outer = 1 / (
-        1 / resistance_convection(tHigh - tLow) + 1 / resistance_radiation(tHigh, tLow)
+        1 / resistance_convection(tHigh - tLow) + 1 / resistance_radiation(tHigh+273.15, tLow+273.15)
     )
 
     return resistance_conduction + resistance_outer
 
 
 # %% Differential equation
-def heatTransfer(Tin, Tout, C):
+def heatTransfer(Tin, Tmid, Tout):
     """
     Parameters
     ----------
@@ -127,24 +131,43 @@ def heatTransfer(Tin, Tout, C):
     qx : Heat flow [W]
     """
     if Tin == Tout:
-        return 0
+        return 0, 0
 
-    qx = (Tout - Tin) / resistance_total(Tin, Tout)
-    dT = qx / C
-    return dT
+    resistance_conduction = (
+        1
+        / (
+            1 / resistance_total_bottom
+            + 1 / resistance_total_lid
+            + 1 / resistance_total_side
+        ).evalf()
+    )
+    
+    resistance_outer = 1 / (
+        1 / resistance_convection(Tmid - Tout) + 1 / resistance_radiation(Tmid+273.15, Tout+273.15)
+    )
+
+    qx1 = (Tmid - Tin) / resistance_conduction
+    qx2 = (Tout - Tmid) / resistance_outer
+    
+    dT1 = qx1 / cWater
+    dT2 = (-qx1 + qx2) / cAlum
+    
+    return dT1, dT2
 
 
-def warmUp(U, I, C):
+def warmUp(U, I):
     q = U * I
-    dT = q / C
+    dT = q / cWater
     return dT
 
 
-def warmUp_heatTransfer(Tin, Tout, U, I, C):
-    return heatTransfer(Tin, Tout, C) + warmUp(U, I, C)
+def warmUp_heatTransfer(Tin, Tmid, Tout, U, I):
+    dT1, dT2 = heatTransfer(Tin, Tmid, Tout)
+    dT1 = dT1  + warmUp(U, I)
+    return dT1, dT2
 
 
-def eulersMethod(de, x0: float, y0: float, h: float, xn: float, *args):
+def eulersMethod(de, x0: float, y0: float, yy0: float, h: float, xn: float, *args):
     """
     Parameters
     ----------
@@ -162,61 +185,68 @@ def eulersMethod(de, x0: float, y0: float, h: float, xn: float, *args):
     """
     x = np.arange(x0, xn, h)
     y = np.zeros(len(x))
+    yy = np.zeros(len(x))
 
     y[0] = y0
+    yy[0] = yy0
 
     for i in range(1, len(x)):
-        y[i] = y[i - 1] + de(y[i - 1], *args) * h
+        dT1, dT2 = de(y[i - 1], yy[i-1], *args)
+        y[i] = y[i - 1] + dT1 * h
+        yy[i] = yy[i - 1] + dT2 * h
 
-    return x, y
+    return x, y, yy
 
 
 # %% Main code
 
 # Thermal resistance and heat capacity for simulation'
 
-mass_water = 3.21  # kg
-heat_capacity = specific_heat_water * mass_water
+mass_water = 3.036  # kg
+cWater = specific_heat_water * mass_water
+cAlum = 900 * 0.02
 
 # Simulation
 start_time = 0
-step_size = 120  # s
-end_time = 60 * 60 * 72  # s
-outside_temperature = 23.4  # C
-start_temperature = outside_temperature  # C
-voltage = 30  # V
-amperage = 6.2  # A
-switchTime = 47.5 * 60  # s
+step_size = 10  # s
+end_time = 60 * 60 * 24 * 5 # s
+outside_temperature = 24.5  # C
+start_temperature = 24.7  # C
+voltage = 27.5  # V
+amperage = 5.9  # A
+switchTime = 35 * 60  # s
 
-t, T = eulersMethod(
+
+t, T, TT = eulersMethod(
     warmUp_heatTransfer,
     start_time,
     start_temperature,
+    outside_temperature,
     step_size,
     switchTime,
     outside_temperature,
     voltage,
     amperage,
-    heat_capacity,
 )
 
-t2, T2 = eulersMethod(
+t2, T2, TT2 = eulersMethod(
     heatTransfer,
     t[-1],
     T[-1],
+    TT[-1],
     step_size,
     end_time,
     outside_temperature,
-    heat_capacity,
 )
 
 t = np.concatenate((t, t2), axis=0)
 T = np.concatenate((T, T2), axis=0)
+TT = np.concatenate((TT, TT2), axis=0)
 
 print("Simulation complete!")
 
 # %% Export data
-df_sim = pd.DataFrame({"t": t, "T": T})
+df_sim = pd.DataFrame({"t": t, "T": T, "TT": TT})
 df_sim.set_index("t").to_csv("sim.csv")
 
 df_param = pd.DataFrame(columns=["param", "val"])
